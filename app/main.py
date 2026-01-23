c9124d925b442ea4f3602195d77c14d23bb152ca
AkshayaVajpeyarr <93993831+art3mis-xo@users.noreply.github.com>
2026-01-24 00:28:44 +0800

    Update import statement for pipeline module

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, UploadFile, Query
 import shutil
 # import chromadb
-from pipeline import extract_text, chunk_text, embed_chunks, save_pdf
+# from pipeline import extract_text, chunk_text, embed_chunks, save_pdf
+from app.pipeline import extract_text, chunk_text, embed_chunks, save_pdf
 # from pipeline import store_in_chroma
 from retrieval import embed_query, search_collection
 from sentence_transformers import SentenceTransformer
